6e691a5f1fe982e25aa87db0aa23c609b036f054
arkariz <muhammadrisky1401@gmail.com>
2023-03-12 15:45:12 +0700

    feat: add httpoverride for window http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:rs_ui/module/side_menu/side_menu.dart';
@@ -5,11 +7,22 @@ import 'package:rs_ui/routes/app_pages.dart';
 import 'package:rs_ui/routes/app_routes.dart';
 import 'package:window_manager/window_manager.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
   windowManager.setFullScreen(true);
-  
+
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const MyApp());
 }
 
