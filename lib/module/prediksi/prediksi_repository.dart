ffbe6683d55e4aad0b47462c88405c5cee65fbd1
arkariz <muhammadrisky1401@gmail.com>
2023-03-12 15:50:57 +0700

    change url

--- lib/module/prediksi/prediksi_repository.dart
+++ lib/module/prediksi/prediksi_repository.dart
@@ -8,9 +8,9 @@ class PrediksiRepository {
 
   Future<Prediksi?> getPrediksi(Map<String, dynamic> body) async {
     final response = await _connect.post(
-      "https://rs_api-1-t4657994.deta.app/prediksi",
+      "http://127.0.0.1:8000/prediksi", //https://rs_api-1-t4657994.deta.app/prediksi",
       body,
-      headers: {"X-Space-App-Key": "U2WB4N67kkUGP3bnLvxYze9zbeDGGbNv"},
+      // headers: {"X-Space-App-Key": "U2WB4N67kkUGP3bnLvxYze9zbeDGGbNv"},
     );
     if (response.isOk) {
       return Prediksi.fromJson(response.body);
@@ -20,7 +20,8 @@ class PrediksiRepository {
         SnackBarUtil.showFlushBarError(Get.context!, response.body['detail']);
         return null;
       } else {
-        SnackBarUtil.showFlushBarError(Get.context!, "Tidak ada koneksi internet");
+        SnackBarUtil.showFlushBarError(
+            Get.context!, "Tidak ada koneksi internet");
         return null;
       }
     }
